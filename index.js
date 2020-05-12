13074fdf3351ef772573dd098e7ca6b0ddf83c97
M3156 <m3156@student.jamk.fi>
2020-05-12 23:07:19 +0300

    3.9 done

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
 
 app.use(express.json())
 app.use(morgan(':method :url :status - :response-time ms :body'))
+app.use(cors())
 
 let persons =  [
     {
