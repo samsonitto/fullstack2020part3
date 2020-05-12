74ba6f45c2d0a11b51b6eac684415f8947e03277
M3156 <m3156@student.jamk.fi>
2020-05-12 18:26:49 +0300

    3.3 done

--- index.js
+++ index.js
@@ -32,6 +32,18 @@ app.get('/info', (req, res) => {
   res.send(content)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if(person) {
+        res.json(person)
+    } else {
+        res.send(`There is no such person`)
+        res.status(404).end()     
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
