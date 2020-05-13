dbeb2fb9da3d898f276e3b1fbea2199d9869cd52
M3156 <m3156@student.jamk.fi>
2020-05-13 16:14:11 +0300

    3.11 done

---
+++ build/precache-manifest.1293f52a5f77d072fa6a197345458362.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd2591a1d97617f2f74d5056ccea1cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0cdfdbdaaf572b9f8f8",
+    "url": "/static/css/main.eedd306b.chunk.css"
+  },
+  {
+    "revision": "25a3600c1dc9b90d5fa2",
+    "url": "/static/js/2.4baeefc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4baeefc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0cdfdbdaaf572b9f8f8",
+    "url": "/static/js/main.6b6978db.chunk.js"
+  },
+  {
+    "revision": "d6d4691db26ca793a81a",
+    "url": "/static/js/runtime-main.4f3ee9ca.js"
+  }
+]);
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
 app.use(express.json())
 app.use(morgan(':method :url :status - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('build'))
 
 let persons =  [
     {
