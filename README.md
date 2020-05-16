7426f5cf6e0b6aa14d3abaea80a16771bedc7fcf
M3156 <m3156@student.jamk.fi>
2020-05-16 12:02:54 +0300

    3.19 done

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ https://fullstack2020phonebook.herokuapp.com/
 | 0   |        3.5        |
 | 1   |         6         |
 | 2   |       11.5        |
-| 3   |         3         |
+| 3   |         6         |
 
 ## Cheatsheet
 
--- modules/person.js
+++ modules/person.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
 
 const url = process.env.MONGODB_URI
 
@@ -15,9 +17,9 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     })
 
 const contactSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-    date: Date
+    name: { type: String, required: true, unique: true },
+    number: { type: String, required: true, unique: true, minlength: 10 },
+    date: { type: Date, required: true }
 })
 
 contactSchema.set('toJSON', {
@@ -27,5 +29,7 @@ contactSchema.set('toJSON', {
         delete returnedObject.__v
     }
 })
+
+contactSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('Person', contactSchema)
